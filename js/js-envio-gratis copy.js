7d62e4a475b6e5672b4e03a8700b92907986cc31
Juan Vanegas <59509327+Juan-cyber1@users.noreply.github.com>
2023-01-13 11:44:31 -0500

    Add files via upload

--- js/js-envio-gratis copy.js
+++ js/js-envio-gratis copy.js
@@ -21,7 +21,7 @@ function limpiarTexto(texto) {
         .toLowerCase();
     return retorno;
 }
-var globalResult = "";
+
 var textoIngresado = "";
 
 const buscarCiudad = async searchText => {
@@ -33,67 +33,41 @@ const buscarCiudad = async searchText => {
             const newResult = new RegExp(`^${limpiarTexto(searchText)}`, 'gi');
             let ciud = resultado.value;
             let ciudLimpio = limpiarTexto(ciud);
+            let depto = resultado.dep;
+            let deptoLimpio = limpiarTexto(depto);
+            let location = `${ciudLimpio}, ${deptoLimpio}`;
             textoIngresado = searchText;
-            let compCiudad = ciudLimpio.match(newResult);
+            let compCiudad = location;
             return compCiudad;
 
         });
-    globalData["resultados"] = compararResultado;
-    globalResult = globalData["resultados"];
-    if (compararResultado == "") {
-        buscarDepto(buscador.value);
-
-    }
-
-
+    
 
     if (searchText.length === 0) {
         compararResultado = [];
+        contInput.innerHTML = `<i class="alk-icon-search-mobile icon-search"></i>`;
     } else if (searchText.length > 2) {
         printRes(compararResultado);
     }
 
 }
 
-const buscarDepto = async searchText => {
-    const resp = await fetch('https://feeds.datafeedwatch.com/75005/0bbd9ded49285714a8e439b27f35f1d72f12ae75.json');
-    const resultadosJson = await resp.json();
-    const resultados = resultadosJson.products;
-    compararResultado = resultados.filter(
-        resultado => {
-            const newResult = new RegExp(`^${limpiarTexto(searchText)}`, 'gi');
-            textoIngresado = searchText;
-            let depto = resultado.dep;
-            let deptoLimpio = limpiarTexto(depto);
-            let compDepto = deptoLimpio.match(newResult);
 
-            return compDepto;
-
-        });
-    globalData["resultados"] = compararResultado;
-    globalResult = globalData["resultados"];
-
-    if (searchText.length === 0) {
-        compararResultado = [];
-    } else if (searchText.length > 2) {
-        printResDepto(compararResultado);
-    }
-}
 if (window.innerWidth <= 992) {
     openModal.addEventListener('click', () => {
         modal.classList.remove("moveDown");
         modal.classList.toggle("moveUp");
-
+    closeModal.addEventListener('click', () => {
+        modal.classList.remove("moveUp");
+        modal.classList.toggle("moveDown");
+    });
         setTimeout(() => {
             buscador.focus();
         }, 1000);
 
     });
 
-    closeModal.addEventListener('click', () => {
-        modal.classList.remove("moveUp");
-        modal.classList.toggle("moveDown");
-    });
+
 } else {
     openModal.addEventListener('click', () => {
         setTimeout(() => {
@@ -102,18 +76,27 @@ if (window.innerWidth <= 992) {
     })
 }
 
-console.log(globalResult);
 
-const printRes = globalResult => {
+
+const printRes = compararResultado => {
 
     listaCoincidencias.innerHTML = "";
     content.innerHTML = "";
     resultSearch.innerHTML = "";
-    contInput.innerHTML = "";
-    if (globalResult.length > 0) {
 
+    let lists = "";
+    if (compararResultado.length > 0) {
+        contInput.innerHTML = `<i class="alk-icon-close icon-search" title="Limpiar campo"></i>`;
         buscador.style.border = "1px solid #004797";
-        const opciones = globalResult.map(card => {
+        contInput.addEventListener('click', () => {
+            buscador.value = "";
+            contInput.innerHTML = `<i class="alk-icon-search-mobile icon-search"></i>`;
+            listaCoincidencias.innerHTML = "";
+            buscador.style.border = "1px solid #004797";
+            buscador.setAttribute("placeholder", "Ingresa de tu municipio");
+            buscador.focus();
+        }, 4000);
+        const opciones = compararResultado.map(card => {
 
             let nameDepto = card.dep;
             let nombreDepto = limpiarTexto(nameDepto);
@@ -121,10 +104,9 @@ const printRes = globalResult => {
             let nombreMunLimpio = limpiarTexto(nombreMunicipio);
             let text = buscador.value;
             let textLimpio = limpiarTexto(text);
-
-            console.log(nombreDepto);
-            if (nombreMunLimpio.indexOf(textLimpio) !== -1) {
-
+            let location = `${nombreMunLimpio}, ${nombreDepto}`;
+            prueba = location.includes(textLimpio);
+            if ( location.includes(textLimpio)) {
                 listaCoincidencias.style.display = "block";
                 listaCoincidencias.innerHTML +=
                     `<span id="${card.value}" class="selectOp"><div class="dropdown-item">
@@ -134,12 +116,13 @@ const printRes = globalResult => {
             </div>
             </div>
             </div></span>`;
-                let lists = listaCoincidencias.querySelectorAll("span");
+                lists = listaCoincidencias.querySelectorAll("span");
 
                 for (let i = 0; i < lists.length; i++) {
                     lists[i].addEventListener('click', selectCard);
 
                     function selectCard() {
+                        contInput.innerHTML = `<i class="alk-icon-search-mobile icon-search"></i>`;
                         let opSelect = lists[i].id;
                         listaCoincidencias.style.display = "none";
                         const cardView = compararResultado.map(i => {
@@ -172,99 +155,37 @@ const printRes = globalResult => {
                     }
                 }
             }
+           
+            
         }).join('');
 
-    }
-}
+    } 
+   if (lists.length === 0) {
 
-
-const printResDepto = globalResult => {
-
-    listaCoincidencias.innerHTML = "";
-    content.innerHTML = "";
-    resultSearch.innerHTML = "";
-    contInput.innerHTML = "";
-    if (globalResult.length > 0) {
-
-        buscador.style.border = "1px solid #004797";
-        const opciones = globalResult.map(card => {
-
-            let nameDepto = card.dep;
-            let nombreDepto = limpiarTexto(nameDepto);
-            let nombreMunicipio = card.value;
-            let nombreMunLimpio = limpiarTexto(nombreMunicipio);
-            let text = buscador.value;
-            let textLimpio = limpiarTexto(text);
-
-            console.log(nombreDepto);
-            if (nombreDepto.indexOf(textLimpio) !== -1) {
-
-                listaCoincidencias.style.display = "block";
-                listaCoincidencias.innerHTML +=
-                    `<span id="${card.value}" class="selectOp"><div class="dropdown-item">
-            <div class="panel panel-default">
-            <div class="panel-heading">
-            <p><i class="alk-icon-pin-generico"></i> ${card.nombre_ciudad}, ${card.dep}</p>
-            </div>
-            </div>
-            </div></span>`;
-                let lists = listaCoincidencias.querySelectorAll("span");
-
-                for (let i = 0; i < lists.length; i++) {
-                    lists[i].addEventListener('click', selectCard);
-
-                    function selectCard() {
-                        let opSelect = lists[i].id;
-                        listaCoincidencias.style.display = "none";
-                        const cardView = compararResultado.map(i => {
-                            let ciudad = i.value;
-
-                            if (opSelect == ciudad) {
-                                let desc = i.desc;
-                                let depto = i.dep;
-                                let diasEntrega = "";
-                                let tiempo_entrega = i.tiempo_entrega;
-                                tiempo_entrega == 1 ? diasEntrega = `<p class="txt-blue">${tiempo_entrega} día hábil a partir de la compra.</p>` : diasEntrega = `<p class="txt-blue">${tiempo_entrega} días hábiles a partir de la compra.</p>`;
-                                let horarioEntrega = i.horario_entrega;
-                                buscador.setAttribute("placeholder", opSelect + ", " + depto);
-                                buscador.value = "";
-
-                                retorno = {
-                                    ciudad,
-                                    depto,
-                                    diasEntrega,
-                                    desc,
-                                    horarioEntrega
-
-                                }
-
-                                pintarCards(retorno);
-                            }
-                        });
-
-
-                    }
-                }
-            }
-        }).join('');
-    } else {
         setTimeout(() => {
             listaCoincidencias.style.display = "block";
             buscador.style.border = "1px solid #DD171B";
-            listaCoincidencias.innerHTML = `<p class="txt-red">No hemos encontrado resultados para: "${textoIngresado}"</p>`;
+            listaCoincidencias.innerHTML = `<div class="cont-no-results text-center">
+            <img class="img img-responsive" src="/medias/no-resultados-envio.png?context=bWFzdGVyfG1ldG9kb3MtZW52aW98MTA4MjB8aW1hZ2UvcG5nfGhjNS9oYWQvMTM1Njg3MDk1MjU1MzQvbm9fcmVzdWx0YWRvc19lbnZpby5wbmd8NDFjYmNlYWQ1YzFmYTRkNmEyZjdkOGY3Yzg5Mzk2Mjc2OWQ2NGI1ZTM0ZjhkMzU0ZmU1ZWEwZDA1ODBhZmNmYQ"/>
+            <div class="txt-no-results text-center">
+            <h4>¡Aún no llegamos a tu destino!</h4>
+            <h4>Seguimos trabajando para ello.</h4>
+            </div>
+            </div>`;
 
-            contInput.innerHTML = `<i class="alk-icon-close icon-borrar" title="Limpiar campo"></i>`;
+            contInput.innerHTML = `<i class="alk-icon-close icon-search" title="Limpiar campo"></i>`;
             contInput.addEventListener('click', () => {
                 buscador.value = "";
-                contInput.innerHTML = "";
+                contInput.innerHTML = `<i class="alk-icon-search-mobile icon-search"></i>`;
                 listaCoincidencias.innerHTML = "";
                 buscador.style.border = "1px solid #004797";
-                buscador.setAttribute("placeholder", "Ingresa de tu municipio o departamento");
+                buscador.setAttribute("placeholder", "Ingresa el nombre de tu municipio");
                 buscador.focus();
             }, 4000);
 
         })
-    }
+    
+   }
 }
 
 
